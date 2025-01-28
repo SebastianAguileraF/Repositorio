90b50c0535eb3b7012500e0ec748077a6bd42f48
Sebastian Aguilera Flores <sebastian.af.2003@gmail.com>
2025-01-27 22:11:32 -0600

    cambio.1

--- programa1.py
+++ programa1.py
@@ -1,18 +1,15 @@
 print("Act 3: Ejercicios Capitulo 1")
 
-´´´
-Ejercicio 1: 
-   inciso c) 
+#Ejercicio 1: 
+#  inciso c) 
 
-Ejercicio 2:
-   Secuencia de intstrucciones diseñadas para que una computadora lleve a cabo una tarea 
+#Ejercicio 2:
+#  Secuencia de intstrucciones diseñadas para que una computadora lleve a cabo una tarea 
 
-Ejercicio 3: 
-   El compilador traduce el codigo antes de ejecutar mientras que el interprete lo traduce en tiempo real; linea por linea 
+#Ejercicio 3: 
+#  El compilador traduce el codigo antes de ejecutar mientras que el interprete lo traduce en tiempo real; linea por linea 
 
-Ejercicio 4: 
-   inciso a)
+#Ejercicio 4: 
+#  inciso a)
 
-Ejercicio 5: 
-   
-´´´
+#Ejercicio 5: 
